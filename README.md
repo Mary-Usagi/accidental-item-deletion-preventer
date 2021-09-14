961bdd0024f4efb6a8728b610f843b46a36e5bf2
Sabrina <github@s-steinhardt.de>
2021-09-14 06:22:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Accidental Item Deletion Preventer
+
+Did you spend a full hour customizing your item, adding a perfect description, animations and maybe even sounds... just to accidentally press the delete button and having to do everything again? 
+
+Not anymore!
+
+### This modules adds a confirmation dialogue before deleting stuff
+* When you click on `Delete Item` or `Delete Effect` from the character sheet, a confirmation dialogue will appear before deleting the item. 
+* For a quantity of more than one for an item, you can also decide how many you want to delete, not just the whole stack. 
+* Can be enabled/disabled from the settings.
+
+## Installation Instructions
+
+In the setup screen, use the URL https://github.com/Mary-Usagi/accidental-item-deletion-preventer/releases/latest/download/module.json to install the module.
+
+
---
+++ main.js
@@ -0,0 +1,110 @@
+/* ------------------------------------ */
+/* Initialize module					*/
+/* ------------------------------------ */
+Hooks.once('ready', function () {
+    console.log('accidental-item-deletion-preventer | Initializing accidental-item-deletion-preventer');
+
+    game.settings.register("accidental-item-deletion-preventer", "CheckDelete", {
+        name: "Show item deletion dialogue?",
+        hint: "When an item is deleted from the character sheet, show a confirmation dialogue before deleting. ",
+        scope: "world",
+        default: true,
+        type: Boolean,
+        config: true,
+    })
+
+    Hooks.on("preDeleteItem", (item, render, ...args) => {
+        if (game.settings.get("accidental-item-deletion-preventer", "CheckDelete")) {
+            let actor = item.parent;
+            let itemId = item.id;
+
+            let quantity = item.data.data.quantity;
+            let text = `<p>Are you sure you want to delete <em>"${item.name}"</em> ?</p>`
+            let deleteAmount = 1;
+            if (item.toDelete) {
+                return true;
+            }
+
+            if (quantity > 1) {
+                text += `
+				    <form class="row form-inline">
+					    <div class="form-group">
+					        <strong>Delete:</strong> <input id="deleteAmountID" type="number" value="${quantity}" style="margin-left: 10px" /> <span style="margin-left: 5px; margin-right:230px"> of ${quantity}</span>
+					    </div>
+				    </form>`
+            }
+
+            let d = new Dialog({
+                // localize this text
+                title: `Do you really want to delete this item?`,
+                content: text,
+                buttons: {
+                    one: {
+                        icon: '<i class="fas fa-check"></i>',
+                        label: "Delete",
+                        callback: async (html) => {
+                            if (quantity > 1) {
+                                deleteAmount = parseInt(html.find("input#deleteAmountID").val());
+                            }
+                            if (deleteAmount >= quantity || quantity == undefined) {
+                                item.toDelete = true;
+                                actor.deleteEmbeddedDocuments("Item", [itemId]);
+                            } else if (deleteAmount > 0) {
+                                let new_quantity = quantity - deleteAmount;
+                                await actor.updateEmbeddedDocuments("Item", [{ _id: itemId, id: itemId, "data.quantity": new_quantity }]);
+                            } else {
+                                return;
+                            }
+                        }
+                    },
+                    two: {
+                        icon: '<i class="fas fa-times"></i>',
+                        label: "Cancel",
+                        callback: () => { return false }
+                    }
+                },
+                default: "one"
+            });
+            d.render(true);
+
+            return false;
+        }
+    });
+
+    Hooks.on("preDeleteActiveEffect", (effect, render, ...args) => {
+        if (game.settings.get("accidental-item-deletion-preventer", "CheckDelete")) {
+            let actor = effect.parent;
+            let effectId = effect.id;
+            if (effect.toDelete) {
+                return true;
+            }
+            let d = new Dialog({
+                // localize this text
+                title: "Do you really want to delete this effect?",
+                content: `<p>Are you sure you want to delete <em>"${effect.data.label}"</em> ?</p>`,
+                buttons: {
+                    one: {
+                        icon: '<i class="fas fa-check"></i>',
+                        label: "Delete",
+                        callback: () => {
+                            effect.toDelete = true;
+                            actor.deleteEmbeddedDocuments("ActiveEffect", [effectId]);
+                        }
+                    },
+                    two: {
+                        icon: '<i class="fas fa-times"></i>',
+                        label: "Cancel",
+                        callback: () => { }
+                    }
+                },
+                default: "one"
+            });
+            d.render(true);
+
+            return false;
+        }
+
+    });
+
+}
+);
